8219ec62823c297c50af9fc1d7ada7318d7b33bb
sharonO13 <77701876+sharonO13@users.noreply.github.com>
2021-02-21 23:23:45 +0000

    Testing githook

--- BasicCalculator/src/main/java/calculatorpkg/CalculatorApp.java
+++ BasicCalculator/src/main/java/calculatorpkg/CalculatorApp.java
@@ -19,6 +19,8 @@ public class CalculatorApp {
 		System.out.println("Value1 " + calculation.getValue1());
 		System.out.println("Value2 " + calculation.getValue2());
 		
+		System.out.println("Testing pre-commit hook");
+		
 	}
 
 }
