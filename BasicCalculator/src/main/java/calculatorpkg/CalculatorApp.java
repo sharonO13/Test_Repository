53fb91f056ad1a909949c1de96a607a272c8b958
sharonO13 <77701876+sharonO13@users.noreply.github.com>
2021-02-22 00:05:17 +0000

    Testing branching

--- BasicCalculator/src/main/java/calculatorpkg/CalculatorApp.java
+++ BasicCalculator/src/main/java/calculatorpkg/CalculatorApp.java
@@ -13,13 +13,15 @@ public class CalculatorApp {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-				
+		
 		Calculator calculation = new Calculator(10,20);
 		
 		System.out.println("Value1 " + calculation.getValue1());
 		System.out.println("Value2 " + calculation.getValue2());
 		
-		System.out.println("Testing Webhooks again");
+		
+		
+		System.out.println("Addition " + calculation.addition());
 		
 	}
 
